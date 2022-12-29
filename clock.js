66bcd96093568b80dfbc2b7a5bf09f059fab84d9
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2022-12-29 16:07:10 +0000

    final (maybe?)

--- clock.js
+++ clock.js
@@ -19,12 +19,13 @@ function setDate() {
     digitalSeconds.innerHTML = secs;
 
 
-    const minutes = now.getMinutes();
+    const minutes = (now.getMinutes()<10?'0':'') + now.getMinutes();
     const minutesAngle = ((minutes/60) * 360) + ((seconds / 60) * 6) + 90;
     minutesHand.style.transform = `rotate(${minutesAngle}deg)`;
     digitalMinutes.innerHTML = minutes;
 
-    const hour = now.getHours();
+    // const hour = now.getHours();
+    const hour = (now.getHours()<10?'0':'') + now.getHours();
     const hourAngle = ((hour / 12) * 360) + ((minutes/60) * 30) + 90;
     hoursHand.style.transform = `rotate(${hourAngle}deg)`;
     digitalHours.innerHTML = hour;
@@ -40,6 +41,7 @@ const analogClock = document.querySelector(".analog-clock");
 const digitClock = document.querySelector(".digital-clock");
 
 const container = document.querySelector(".container");
+const container2 = document.querySelector(".container-2");
 
 const textAD = document.querySelector(".toggle span");
 
@@ -49,18 +51,31 @@ let active = false
 function toggle() {
     let toggle = document.querySelector('.toggle');
     active = !active;
+
+    var width = window.innerWidth;
+
     if (active) {
         toggle.classList.add('active');
         analogClock.style.visibility = "hidden";
         digitClock.style.visibility = "visible";
-        container.style.padding = "8rem 3rem";
         textAD.innerHTML = "Digital";
+        if(width<= 600){
+            container.style.padding = "5rem 4rem";
+            container2.style.top = "10%";
+        }else{
+            container.style.padding = "8rem 3rem";
+        }
     } else {
         toggle.classList.remove('active');
         analogClock.style.visibility = "visible";
         digitClock.style.visibility = "hidden";
-        container.style.padding = "17rem 0";
         textAD.innerHTML = "Analog";
+        if(width<=600){
+            container.style.padding = "17rem 3.5rem";
+            container2.style.top = "5%";
+        }else{
+            container.style.padding = "17rem 0";
+        }
     }
 };
 
